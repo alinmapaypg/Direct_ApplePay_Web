af1d38043c978760d7a18c469f8cd729a87a3d9c
Riyaztechsupport <135695828+Riyaztechsupport@users.noreply.github.com>
2026-02-12 16:17:16 +0300

    Update apple_pay_conf.php

--- apple_pay_conf.php
+++ apple_pay_conf.php
@@ -8,7 +8,7 @@ define('PRODUCTION_CERTIFICATE_KEY_PASS', '123456');
 
 //define('PRODUCTION_MERCHANTIDENTIFIER', openssl_x509_parse( file_get_contents( PRODUCTION_CERTIFICATE_PATH ))['subject']['UID'] ); //if you have a recent version of PHP, you can leave this line as-is. http://uk.php.net/openssl_x509_parse will parse your certificate and retrieve the relevant line of text from it e.g. merchant.com.name, merchant.com.mydomain or merchant.com.mydomain.shop
 // if the above line isn't working for you for some reason, comment it out and uncomment the next line instead, entering in your merchant identifier you created in your apple developer account
-define('PRODUCTION_MERCHANTIDENTIFIER', 'merchant.vegaah.pg.com');
+define('PRODUCTION_MERCHANTIDENTIFIER', 'merchant.pg.com');
 
 define('PRODUCTION_DOMAINNAME', $_SERVER["HTTP_HOST"]); //you can leave this line as-is too, it will take the domain from the server you run it on e.g. shop.mydomain.com or mydomain.com
 // if the line above isn't working for you, replace it with the one below, updating it for your own domain name
